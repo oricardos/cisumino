5df6a32d05c78a7e69e8980fee3897b02bcbbcb7
oricardos <ricardo.rs95@gmail.com>
2024-04-19 19:15:26 -0300

    Annotation e Inference 1

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+"use strict";
+// aula 01
+// function sum(a: number, b: number){
+//     return a + b;
+// }
+// console.log(sum(1,2))
+//aula 02
+// Annotation e Inference 1
+let produto = "celular";
+let preco = 2000;
+const carro = {
+    nome: "carro",
+    preco: 20000,
+};
+const cheap = preco < 4000 ? true : "produto caro";
 function sum(a, b) {
     return a + b;
 }
-console.log(sum(1, 2));
+sum(4, 3); // isso da certo
+// sum("a", "b") // isso da erro
--- script.ts
+++ script.ts
@@ -1,5 +1,27 @@
-function sum(a, b){
-    return a + b;
+// aula 01
+// function sum(a: number, b: number){
+//     return a + b;
+// }
+// console.log(sum(1,2))
+
+//aula 02
+// Annotation e Inference 1
+let produto: string = "celular";
+let preco: number = 2000;
+
+const carro: {
+  nome: string;
+  preco: number;
+} = {
+  nome: "carro",
+  preco: 20000,
+};
+
+const cheap: boolean | string = preco < 4000 ? true : "produto caro";
+
+function sum(a: number, b: number) {
+  return a + b;
 }
 
-console.log(sum(1,2))
+sum(4,3) // isso da certo
+// sum("a", "b") // isso da erro
